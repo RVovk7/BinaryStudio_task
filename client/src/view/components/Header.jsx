9c338495ee4bad19897087136460ea8d6003861a
RVovk7 <RVovk7@gmail.com>
2018-07-16 01:27:10 +0300

    layout fix

---
+++ client/src/view/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="headText">
+                    <h3>CookBook</h3>
+                </div>
+            </header>
+        );
+    }
+}
